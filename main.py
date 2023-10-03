e0cfc47a73102ac77450c75d9e06f6a338f99003
David Pol <davidpolsanchezmartos@gmail.com>
2023-10-03 17:38:06 +0700

    :see_no_evil: Update gitignore file
    no shity things

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ app = FastAPI()
 
 
 @app.get("/")
-def read_root():
+def index():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/about")
+def about():
+    return {"data": "About Page" }
